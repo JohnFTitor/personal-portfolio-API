7dadce32f0a711d1dca01f78d270ee7320a8cbea
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-06-29 11:40:04 -0500

    Define projects seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'ffaker'
+
+10.times.map do 
+  project = Project.new(
+    title: FFaker::Lorem.characters(10),
+    tags: 5.times.map { FFaker::Lorem.characters(5) },
+    description: FFaker::Lorem.characters(100),
+    source: FFaker::Lorem.characters(20),
+    live: FFaker::Lorem.characters(20),
+    demo: FFaker::Lorem.characters(20)
+  )
+  
+  project.images.attach(io: File.open('storage/images/desktop.png'), filename: 'desktop.png', content_type: 'png')
+  project.images.attach(io: File.open('storage/images/mobile.jpg'), filename: 'mobile.jpg', content_type: 'jpg')
+  
+  project.save!
+end
