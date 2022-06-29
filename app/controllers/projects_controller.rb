e82894416dac71df2d1e3a7103cb3af307d9514b
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-06-29 16:52:44 -0500

    Define create action for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,8 +6,24 @@ class ProjectsController < ApplicationController
     render jsonapi: projects, class: { Project: SerializableProject }
   end
 
+  def create
+    unless @current_user.role == 'admin'
+      return render json: { error: 'No admin user. Unauthorized request' },
+                    status: 401
+    end
+
+    ctx = Projects::Create.call({project_params: })
+
+    return render json: ctx[:errors], status: ctx[:status] if ctx.failure?
+
+    render jsonapi: ctx[:message], status: ctx[:status], class: { Project: SerializableProject }
+  end
+
   def destroy
-    return render json: { error: 'No admin user. Unauthorized request' }, status: 401 unless @current_user.role === 'admin'
+    unless @current_user.role == 'admin'
+      return render json: { error: 'No admin user. Unauthorized request' },
+                    status: 401
+    end
 
     begin
       project = Project.find(params[:id])
@@ -16,5 +32,11 @@ class ProjectsController < ApplicationController
     rescue ActiveRecord::RecordNotFound => e
       render json: { errors: e.message }, status: 404
     end
+  end
+
+  private
+
+  def project_params
+    params.permit(:title, :desktop_image, :mobile_image, :tags, :description, :live, :source, :demo)
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -5,7 +5,7 @@ class Project < ApplicationRecord
   has_many_attached :images, dependent: :destroy
 
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
-  validates :images, presence: true, length: { is: 2 }
+  validates :images, presence: true
   validates :tags, presence: true
   validates :description, presence: true, length: { minimum: 100, maximum: 5000 }
   validates :live, presence: true
