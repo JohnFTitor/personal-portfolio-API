db28692120a2bef6b1fec9658fca2fab93e375b7
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-06-29 15:47:47 -0500

    Add authentication controller to handle login

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :projects, only: [:index]
+  post '/login', to: 'authentication#login'
 end
