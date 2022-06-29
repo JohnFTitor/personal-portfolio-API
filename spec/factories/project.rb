174da2eebc08646f78e25307ac9a938130530f64
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-06-29 11:08:35 -0500

    Add factory bot for project model

---
+++ spec/factories/project.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do 
+  factory :project, class: 'Project' do
+    title { FFaker::Lorem.characters(10) }
+    images { [Rack::Test::UploadedFile.new('spec/assets/test.png', 'image/png'), Rack::Test::UploadedFile.new('spec/assets/test.png', 'image/png')] }
+    tags { 5.times.map { FFaker::Lorem.characters(5) } }
+    description { FFaker::Lorem.characters(100) }
+    live { FFaker::Lorem.characters(20) }
+    source { FFaker::Lorem.characters(20) }
+    demo { FFaker::Lorem.characters(20) }
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'ffaker'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -38,6 +39,7 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+  config.include FactoryBot::Syntax::Methods
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
