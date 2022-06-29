643110071b275949ba51791ac78bfd7f2b29bea9
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-06-29 16:12:26 -0500

    Define destroy action

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,19 @@ class ProjectsController < ApplicationController
   skip_before_action :authorize_request, only: [:index]
 
   def index
-    projects = Project.all
+    projects = Project.all.order(created_at: :desc)
     render jsonapi: projects, class: { Project: SerializableProject }
+  end
+
+  def destroy
+    return render json: { error: 'No admin user. Unauthorized request' }, status: 401 unless @current_user.role === 'admin'
+
+    begin
+      project = Project.find(params[:id])
+      project.destroy
+      render json: { message: 'Project successfully deleted' }, status: 200
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: 404
+    end
   end
 end
