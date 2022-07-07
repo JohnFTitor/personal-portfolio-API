da09243cbd7cb06a58e3de2bd901ae4aa12cb5ae
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-07-07 11:30:03 -0500

    Modify seed to meet data formar

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,12 +9,12 @@ require 'ffaker'
 
 10.times.map do 
   project = Project.new(
-    title: FFaker::Lorem.characters(10),
+    title: FFaker::Lorem.words(3).join(' '),
     tags: 5.times.map { FFaker::Lorem.characters(5) },
-    description: FFaker::Lorem.characters(100),
+    description: FFaker::Lorem.words(200).join(' '),
     source: FFaker::Lorem.characters(20),
     live: FFaker::Lorem.characters(20),
-    demo: FFaker::Lorem.characters(20)
+    demo: 'https://www.youtube.com/embed/dQw4w9WgXcQ',
   )
   
   project.images.attach(io: File.open('storage/images/desktop.png'), filename: 'desktop.png', content_type: 'png')
