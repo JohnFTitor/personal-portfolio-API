c51014a404cc5fd738a0a6e53bad031dfd27dc5c
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-07-09 18:07:36 -0500

    Add host to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,6 +71,9 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.action_mailer.default_url_options = { host: 'johnftitor-portfolio-api.herokuapp.com' }
+  Rails.application.routes.default_url_options[:host] = 'johnftitor-portfolio-api.herokuapp.com'
+
   # Use a different logger for distributed setups.
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
